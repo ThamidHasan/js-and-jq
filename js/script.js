6b1de2440e9213ac05610cf8bcc309742b00b0ea
Thamid Hasan <90929601+ThamidHasan@users.noreply.github.com>
2021-12-14 07:23:41 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function textChange(){
+	document.getElementById('p').innerHTML="Text chang by Id selector";
+}
+function textChangeClass(){
+	document.getElementsByClassName('change')[1].innerHTML= "Text chang by Class selector";
+}
+function textChangeDate(){
+	document.getElementById('date').innerHTML=Date();
+}
+function styleChange(){
+	document.getElementById('text_style').style.color = "red";
+	document.getElementById('text_style1').style.fontSize = "100px";
+	document.getElementById('text_style2').style.border = "1px solid red";
+}
+function imgOne(){
+	document.getElementById('img').src = "img/work2.jpg";
+}
+function imgTwo(){
+	document.getElementById('img').src = "img/work3.jpg";
+}
+
+$(document).ready(function(){
+	alert("Welcome to jQuery Practice")
+
+	//hide
+	$('.hide').click(function(){
+		$('img').hide("fast");
+	});
+
+	//show
+	$('#show').click(function(){
+		$('img').show(1000);
+	});
+
+	//Toggle
+	$('#toggle').click(function(){
+		$('img').toggle("slow");
+	});
+});
